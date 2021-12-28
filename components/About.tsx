d2dd498c5014a2c2b1e97d3ae2c4bf8246958645
Malza0408 <parkjh04080@gmail.com>
2021-12-28 22:22:36 +0900

    add About

--- components/About.tsx
+++ components/About.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { faCss3, faCss3Alt, faJs } from "@fortawesome/free-brands-svg-icons";
+import {
+  faCss3,
+  faCss3Alt,
+  faJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled, { css, DefaultTheme } from "styled-components";
 
@@ -16,19 +21,71 @@ const IconContainer = styled.div`
   justify-content: space-around;
 `;
 
+const Icon_blobs = styled.div`
+  height: 100%;
+  border-radius: 50%;
+  position: absolute;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  right: 0px;
+  z-index: -1;
+`;
+
+const Blobs_item = styled.div`
+  ${({ theme }) => {
+    return css`
+      background-color: ${theme.colors.lightBlue};
+    `;
+  }}
+  width: 45%;
+  height: 100%;
+  border-radius: 100%;
+  position: absolute;
+  transform: scale(1.4) translateY(125%);
+  transition: all 0.7s ease;
+
+  &:nth-child(1) {
+    left: -5%;
+  }
+  &:nth-child(2) {
+    left: 30%;
+    transition-delay: 0.06s;
+  }
+  &:nth-child(3) {
+    left: 66%;
+    transition-delay: 0.025s;
+  }
+`;
+
 const IconOutline = styled.div`
   width: 15rem;
   height: 15rem;
-  margin: 2rem auto;
+  margin: 4rem auto 2rem auto;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+  transition: all 700ms ease;
   ${({ theme }) => {
     return css`
       border: 0.1rem solid ${theme.colors.lightBlue};
       border-radius: 50%;
+      color: ${theme.colors.lightBlue};
     `;
   }}
+
+  &:hover {
+    ${({ theme }) => {
+      return css`
+        color: ${theme.colors.white};
+      `;
+    }}
+  }
+
+  &:hover > ${Icon_blobs} > ${Blobs_item} {
+    transform: scale(1.4) translateY(0) translateZ(0);
+  }
 `;
 
 const StackTitle = styled.p`
@@ -55,15 +112,25 @@ const About = () => {
       <IconContainer>
         <div>
           <IconOutline>
-            <FontAwesomeIcon icon={faJs} size="4x" color="lightblue" />
+            <FontAwesomeIcon icon={faJs} size="4x" />
+            <Icon_blobs>
+              <Blobs_item />
+              <Blobs_item />
+              <Blobs_item />
+            </Icon_blobs>
           </IconOutline>
           <StackTitle>HTML, CSS, JS</StackTitle>
         </div>
         <div>
           <IconOutline>
-            <FontAwesomeIcon icon={faJs} size="4x" color="lightblue" />
+            <FontAwesomeIcon icon={faReact} size="4x" />
+            <Icon_blobs>
+              <Blobs_item />
+              <Blobs_item />
+              <Blobs_item />
+            </Icon_blobs>
           </IconOutline>
-          <StackTitle>HTML, CSS, JS</StackTitle>
+          <StackTitle>React</StackTitle>
         </div>
       </IconContainer>
     </AboutSection>
